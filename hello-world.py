8df4c06b99cac6dbe5e9b4b62605578cc742445e
AndreAledia <aaledia@ucsc.edu>
2025-10-04 19:42:54 -0700

    Init

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+# Print "Hello World" to the console
+def PrintHelloWorld():
+    print("Hello World")
+    
+# Main function to execute the print function
+def main():
+    PrintHelloWorld()
+    
+# When we run the program, call the main function
+if __name__ == "__main__":
+    main()
---
+++ intro.py
@@ -0,0 +1,15 @@
+# Print "Intro" to the console
+def PrintIntro():
+    print("Andre Aledia")
+    print("He/Him")
+    print("My favorite movie is Top Gun.")
+    print("""My favorite food is pork katsu. 
+Though, the best food I've ever tried was beef tongue.""")
+
+# Main function to execute the print function
+def main():
+    PrintIntro()
+
+# When we run the program, call the main function
+if __name__ == "__main__":
+    main()
